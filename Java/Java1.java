1f53f2d526a944c9caa3fb89de3aafdb6b6f3cd2
Msf_Snc <mustafasineci2004@gmail.com>
2024-05-04 12:16:50 +0300

    Java Lesson

---
+++ Java/Java1.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Java1 {
+    public static void main(String[] args) {
+        System.out.println("Lütfen 1-10 arasında bir sayı giriniz.");
+        Random random = new Random(); // Tr: Random sınıfından bir nesne oluşturur. // En: Creates an object of class Random.
+        int guessNumber = random.nextInt(10) + 1; // Tr: 1 ile 10 arasında random sayı oluşturma  // En: Generate a random number between 1 and 10
+        Scanner scanner = new Scanner(System.in); // Tr: Scanner sınıfından bir nesne oluşturur. // En: Creates an object of class Scanner.
+        System.out.print(">>");
+        int number = scanner.nextInt(); // Tr: Kullanıcıdan veri alma işlemi // En: Retrieving data from the user
+        // Tr: 1. Tahmin işlemi // En: 1. Estimation process
+            if (number > guessNumber) {
+                System.out.println("Lütfen daha küçük bir sayı giriniz. Kalan tahmin hakkınız:3");
+                System.out.print(">>");
+                number = scanner.nextInt();
+            } else if (number < guessNumber) {
+                System.out.println("Lütfen daha büyük bir sayı giriniz. Kalan tahmin hakkınız:3");
+                System.out.print(">>");
+                number = scanner.nextInt();
+            }else if (number == guessNumber){
+                System.out.println("Sayıyı buldunuz. Kazandınız.");
+            }
+        // Tr: 2. Tahmin işlemi // En: 2. Estimation process
+            if (number > guessNumber) {
+                System.out.println("Lütfen daha küçük bir sayı giriniz. Kalan tahmin hakkınız:2");
+                System.out.print(">>");
+                number = scanner.nextInt();
+            } else if (number < guessNumber) {
+                System.out.println("Lütfen daha büyük bir sayı giriniz. Kalan tahmin hakkınız:2");
+                System.out.print(">>");
+                number = scanner.nextInt();
+            }else if (number == guessNumber){
+                System.out.println("Sayıyı buldunuz. Kazandınız.");
+            }
+        // Tr: 3. Tahmin işlemi // En: 3. Estimation process
+            if (number > guessNumber) {
+                System.out.println("Lütfen daha küçük bir sayı giriniz. Kalan tahmin hakkınız:1");
+                System.out.print(">>");
+                number = scanner.nextInt();
+            } else if (number < guessNumber) {
+                System.out.println("Lütfen daha büyük bir sayı giriniz. Kalan tahmin hakkınız:1");
+                System.out.print(">>");
+                number = scanner.nextInt();
+            }else if (number == guessNumber){
+                System.out.println("Sayıyı buldunuz. Kazandınız.");
+            }
+
+        // Tr: SON TAHMİN HAKKI OLDUĞU İÇİN DAHA FAZLA İŞLEME İZİN VERİLMİYOR
+        // En: NO FURTHER PROCESSING IS ALLOWED AS IT IS THE LAST GUESS
+            if (number == guessNumber) {
+                System.out.println("Sayıyı buldunuz. Kazandınız.");
+            } else {
+                System.out.println("Kaybettiniz Bilemediğiniz sayi:"+ guessNumber);
+                // (Bilemediginiz sayi:"+ guessNumber) Üsame abiden çaldım :)
+            }
+    }
+}
---
+++ Java/Java2.java
@@ -0,0 +1,14 @@
+public class Java2 {
+    public static void main(String[] args) {
+        /*
+        Exam: Identify two numbers with integer values 5 and 12. Then, assign them to a variable
+        named "result" and print the result on the screen. The possible output should be as
+        follows. 5*12=60
+        */
+    int num1 = 5;
+    int num2 = 12;
+    int result;
+    result = num1 + num2;
+    System.out.println(num1 + "x" + num2 + "=" + result);
+}
+}
